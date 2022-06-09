a01da6db8fb56f963af695380b372e85b1de5e64
Fawaz Mohamed <fwz.mhmd@gmail.com>
2022-06-09 23:27:42 +0100

    Rewrite js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,52 @@
 import config from '../config.json' assert { type: 'json' };
+import { displayResults } from './utils.js';
 
-const api = `http://www.omdbapi.com/?apikey=${config.api_key}&type=movie&r=json&s=`;
+const api = `http://www.omdbapi.com/?apikey=${config.api_key}&type=movie`;
 const form = document.querySelector('form');
 const input = document.querySelector('form input');
+const pageResults = [];
 
 const content = document.querySelector('#content');
 
-form.addEventListener('submit', e => {
+form.addEventListener('submit', search);
+
+// take input
+function search(e) {
   e.preventDefault();
-  const inputValue = input.value;
-  if (inputValue) {
-    fetch(api + inputValue)
-      .then(res => res.json())
-      .then(data => showResult(data));
-  }
+  const value = input.value;
+  content.innerHTML = '';
+  searchApi(value);
   form.reset();
+}
+
+// search input
+async function searchApi(value) {
+  const res = await fetch(`${api}&s=${value}`);
+  const data = await res.json();
+
+  console.log(data);
+  listMovies(data);
+}
+
+async function listMovies(data) {
+  const movies = await data.Search;
+  console.log(movies);
+  movies.forEach(async movie => {
+    const res = await fetch(`${api}&i=${movie.imdbID}`);
+    const data = await res.json();
+    displayResults(data);
+  });
+}
+
+content.addEventListener('click', e => {
+  const target = e.target;
+  if (target.nodeName === 'BUTTON') {
+    fetch(`${api}&i=${target.id}`)
+      .then(res => res.json())
+      .then(data =>
+        window.localStorage.setItem(target.id, JSON.stringify(data))
+      );
+  }
 });
 
-function showResult(obj) {
-  const { Response, Error } = obj;
-  console.log(obj);
-  if (Response === 'True') {
-    const results = obj.Search.map(result => result);
-    results.forEach(r => {
-      const { Title, imdbRating, Year, Runtime, Genre, Plot, Poster } = r;
-
-      const card = `<div class="movie-card">
-          <div class="poster">
-            <img
-              src="${Poster}"
-              alt=""
-            />
-          </div>
-          <div class="movie-info">
-            <div class="top">
-              <p class="movie-title">${Title} (${Year})</p>
-              <p class="movie-rating"><i class="fa-solid fa-star"></i> ${imdbRating}</p>
-              <button class="btn-add">
-                <i class="fa-solid fa-plus"></i><span>Watchlist</span>
-              </button>
-            </div>
-            <div class="middle">
-              <p class="movie-runtime">${Runtime}</p>
-              <div class="movie-tags">
-              <p>Help</p>
-              </div>
-            </div>
-            <div class="summary">
-              <p>
-                ${Plot}
-              </p>
-            </div>
-          </div>
-        </div>`;
-      // content.innerHTML = card;
-    });
-  } else {
-    console.log(Error);
-  }
-}
-
-function addHere() {
-  return document.querySelector('.add-btn');
-}
+export { pageResults };
